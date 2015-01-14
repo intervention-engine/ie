e647abd740eb7f57e99735bed8ea19cfac197c27
Andy Gregorowicz <andy@mitre.org>
2015-01-14 16:37:28 -0500

    Refactoring to use the new Load methods in the fhir package
    This eliminates the need for the loadQuery function.

--- controllers/query_totals.go
+++ controllers/query_totals.go
@@ -5,16 +5,17 @@ import (
 
 	"net/http"
 
-	"github.com/gorilla/mux"
-	fhirmodels "github.com/intervention-engine/fhir/models"
 	"github.com/intervention-engine/fhir/server"
 	"github.com/intervention-engine/ie/middleware"
 	"github.com/intervention-engine/ie/models"
-	"gopkg.in/mgo.v2/bson"
 )
 
 func ConditionTotalHandler(rw http.ResponseWriter, r *http.Request) {
-	query := loadQuery(rw, r)
+	query, err := server.LoadQuery(r)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	pipeline := models.CreateConditionPipeline(query)
 	factCollection := server.Database.C("facts")
 	qr := &middleware.QueryResult{}
@@ -23,31 +24,14 @@ func ConditionTotalHandler(rw http.ResponseWriter, r *http.Request) {
 }
 
 func EncounterTotalHandler(rw http.ResponseWriter, r *http.Request) {
-	query := loadQuery(rw, r)
+	query, err := server.LoadQuery(r)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	pipeline := models.CreateEncounterPipeline(query)
 	factCollection := server.Database.C("facts")
 	qr := &middleware.QueryResult{}
 	factCollection.Pipe(pipeline).One(qr)
 	json.NewEncoder(rw).Encode(qr)
-}
-
-func loadQuery(rw http.ResponseWriter, r *http.Request) *fhirmodels.Query {
-	var id bson.ObjectId
-
-	idString := mux.Vars(r)["id"]
-	if bson.IsObjectIdHex(idString) {
-		id = bson.ObjectIdHex(idString)
-	} else {
-		http.Error(rw, "Invalid id", http.StatusBadRequest)
-	}
-
-	c := server.Database.C("querys")
-
-	query := &fhirmodels.Query{}
-	err := c.Find(bson.M{"_id": id.Hex()}).One(query)
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return nil
-	}
-	return query
 }
