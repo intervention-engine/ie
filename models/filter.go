2fb5143a57a010b707cea74956aa0dc2904784bd
Andy Gregorowicz <andy@mitre.org>
2015-01-16 10:17:22 -0500

    whitespace cleanup

--- models/filter.go
+++ models/filter.go
@@ -32,8 +32,8 @@ type Pane struct {
 }
 
 type EmberItem struct {
-	id         			string               `json:"-" bson:"_id"`
-	Parameter  			fhirmodels.Extension `json:"parameter" bson:"parameter"`
-	Active     			*bool                `json:"active" bson:"active"`
-	ComponentName   string               `json:"componentName" bson:"componentName"`
+	id            string               `json:"-" bson:"_id"`
+	Parameter     fhirmodels.Extension `json:"parameter" bson:"parameter"`
+	Active        *bool                `json:"active" bson:"active"`
+	ComponentName string               `json:"componentName" bson:"componentName"`
 }
