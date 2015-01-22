1dcff400842605c8d9344f8fadf2a93311d0d46f
Andrew Hubley <ahubley@mitre.org>
2015-01-22 12:51:16 -0500

    initial handlers for login/register

---
+++ models/user.go
@@ -0,0 +1,47 @@
+package models
+
+import (
+  "golang.org/x/crypto/bcrypt"
+  "gopkg.in/mgo.v2/bson"
+  "github.com/intervention-engine/fhir/server"
+  )
+
+type User struct {
+  ID bson.ObjectId `bson:"_id,omitempty"`
+  Username string
+  Password []byte
+}
+
+// type UserContext struct {
+//   Session sessions.Session
+// }
+// 
+// func NewUserContext(req *http.Request) (*UserContext, error) {
+//   sess, err := Store.Get(req, "intervention-engine")
+//   return &UserContext {
+//     Sessions = sess
+//   }, err
+// }
+
+func (u *User) SetPassword(password string) {
+  hashedpass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+  if err != nil {
+    panic(err)
+  }
+
+  u.Password = hashedpass
+}
+
+func Login(username, password string) (u *User, err error) {
+  err = server.Database.C("users").Find(bson.M{"username": username}).One(&u)
+  if err != nil {
+    return
+  }
+
+  err = bcrypt.CompareHashAndPassword(u.Password, []byte(password))
+  if err != nil {
+    u = nil
+  }
+
+  return u, err
+}
