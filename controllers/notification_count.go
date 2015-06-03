e51ca68a10b9b1a32c85f64ee5e848140366fea7
Chris Moesel <cmoesel@mitre.org>
2015-06-03 12:38:54 -0400

    Add support for querying all notification counts

---
+++ controllers/notification_count.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"encoding/json"
+	"github.com/intervention-engine/fhir/server"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+	"net/http"
+)
+
+func NotificationCountHandler(rw http.ResponseWriter, r *http.Request) {
+	pipe := server.Database.C("communicationrequests").Pipe([]bson.M{{"$group": bson.M{"_id": "$subject.referenceid", "count": bson.M{"$sum": 1}}}})
+	var results []struct {
+		Patient string  `bson:"_id" json:"patient"`
+		Count   float64 `bson:"count" json:"count"`
+	}
+	err := pipe.All(&results)
+	if err != nil {
+		log.Printf("Error getting notification count: %#v", err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	} else {
+		json.NewEncoder(rw).Encode(results)
+	}
+}
