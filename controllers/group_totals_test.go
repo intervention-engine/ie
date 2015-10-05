7df48167558e0cd9dac34e4c5687339a08812715
Andrew Hubley <ahubley@mitre.org>
2015-10-05 16:22:54 -0400

    fixtures and refactor of test suite - not passing yet

--- controllers/group_totals_test.go
+++ controllers/group_totals_test.go
@@ -1,19 +1,17 @@
 package controllers
 
 import (
-	"bufio"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
 
+	fhirmodels "github.com/intervention-engine/fhir/models"
 	"github.com/intervention-engine/fhir/server"
-	"github.com/intervention-engine/ie/models"
 	"github.com/pebbe/util"
 	. "gopkg.in/check.v1"
-	"gopkg.in/mgo.v2/bson"
 	"gopkg.in/mgo.v2/dbtest"
-	"strings"
 )
 
 type QueryTotalsSuite struct {
@@ -28,24 +26,44 @@ func (q *QueryTotalsSuite) SetUpSuite(c *C) {
 }
 
 func (q *QueryTotalsSuite) SetUpTest(c *C) {
-	file, err := os.Open("../fixtures/facts.json")
-	defer file.Close()
+	patientFile, err := os.Open("../fixtures/patient-example-a.json")
+	defer patientFile.Close()
+	util.CheckErr(err)
+
+	encounterFile, err := os.Open("../fixtures/encounter-example.json")
+	defer encounterFile.Close()
+	util.CheckErr(err)
+
+	conditionFile, err := os.Open("../fixtures/condition-example.json")
+	defer conditionFile.Close()
 	util.CheckErr(err)
 
 	// Setup the database
 	session := q.DBServer.Session()
-	factCollection := session.DB("ie-test").C("facts")
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		decoder := json.NewDecoder(strings.NewReader(scanner.Text()))
-		fact := &models.Fact{}
-		err = decoder.Decode(fact)
-		util.CheckErr(err)
-		i := bson.NewObjectId()
-		fact.Id = i.Hex()
-		factCollection.Insert(fact)
-	}
-	util.CheckErr(scanner.Err())
+
+	patientCollection := session.DB("ie-test").C("patients")
+	encounterCollection := session.DB("ie-test").C("encounters")
+	conditionCollection := session.DB("ie-test").C("conditions")
+
+	patientDecoder := json.NewDecoder(patientFile)
+	encounterDecoder := json.NewDecoder(encounterFile)
+	conditionDecoder := json.NewDecoder(conditionFile)
+
+	patient := &fhirmodels.Patient{}
+	encounter := &fhirmodels.Encounter{}
+	condition := &fhirmodels.Condition{}
+
+	err = patientDecoder.Decode(patient)
+	util.CheckErr(err)
+	err = encounterDecoder.Decode(encounter)
+	util.CheckErr(err)
+	err = conditionDecoder.Decode(condition)
+	util.CheckErr(err)
+
+	patientCollection.Insert(patient)
+	encounterCollection.Insert(encounter)
+	conditionCollection.Insert(condition)
+
 	server.Database = session.DB("ie-test")
 }
 
@@ -70,9 +88,10 @@ func (q *QueryTotalsSuite) TestInstaCountAllHandler(c *C) {
 
 	counts := make(map[string]int)
 	err := json.NewDecoder(w.Body).Decode(&counts)
+
 	util.CheckErr(err)
 
-	c.Assert(counts["patients"], Equals, 5)
-	c.Assert(counts["conditions"], Equals, 10)
-	c.Assert(counts["encounters"], Equals, 12)
+	c.Assert(counts["patients"], Equals, 1)
+	c.Assert(counts["conditions"], Equals, 1)
+	c.Assert(counts["encounters"], Equals, 1)
 }
